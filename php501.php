b45815b5ce57e08776f6549e204f33d9e1c9d30f
Jimmyseesang <connors.svf0683@gmail.com>
2021-12-03 11:34:31 +0700

    initial commit

---
+++ php501.php
@@ -0,0 +1,29 @@
+<?php
+
+$season = array("summer","winter","spring","autumn");
+
+echo $season[0];
+echo "<br/>";
+echo $season[1];
+echo "<br/>";
+echo $season[2];
+echo "<br/>";
+echo $season[3];
+echo "<br/>";
+?>
+
+<?php
+$country = array("Thailand","Malaysia","Singapore");
+echo"========================";
+echo "<br/>";
+$country[0] = "Thailand";
+$country[1] = "Malaysia";
+$country[2] = "Singapore";
+echo $country[0];
+echo "<br/>";
+echo $country[1];
+echo "<br/>";
+echo $country[2];
+for(int i=0;i<=3;){
+    
+}
---
+++ php502.php
@@ -0,0 +1,13 @@
+<?hph
+$season = array("summer","winter","spring","autumn");
+echo $count = count($season);
+echo "<br/>";
+for ($i = 0;$i <= 0;$i=$i+1){
+    echo $season[$i];
+    echo "<br/>";
+}
+
+foreach ($season as $i){
+    echo $i;
+    echo "<br/>";
+}
